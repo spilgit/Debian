c56db20f8ff7b922b4d0410286c693f7581f48d5
KHMF Technologies <spilgit@users.noreply.github.com>
2016-09-12 15:24:12 -0400

    apache.sh

---
+++ apache.sh
@@ -0,0 +1,20 @@
+last
+df -h
+apt-get -y install apache2
+apt-get -y install php5 libapache2-mod-php5
+service apache2 restart
+nano /var/www/html/info.php
+ifconfig
+apt-cache search php5
+apt-get -y install php5-mysqlnd php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+service apache2 restart
+apt-get install php5-apcu
+service apache2 restart
+apt-get -y install mysql-server
+apt-get -y install phpmyadmin
+find / -name php.ini
+nano /etc/php5/apache2/php.ini
+ls
+mkdir Documents
+cd Documents
+history | cut -c 8- > history09122016.txt
